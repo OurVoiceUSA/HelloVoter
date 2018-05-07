341b644e0f01020437fedef86a06b368cc2dcfb2
Corey Henderson <corman@cormander.com>
2018-05-06 20:46:01 -0600

    update markers after setstate

--- src/components/CanvassingPage/index.js
+++ src/components/CanvassingPage/index.js
@@ -520,7 +520,6 @@ export default class App extends PureComponent {
     let rmshare = false;
 
     if (this.state.canvassSettings.share_progress !== canvassSettings.share_progress && canvassSettings.share_progress === false) rmshare = true;
-    if (this.state.canvassSettings.show_only_my_turf !== canvassSettings.how_only_my_turf) this.updateMarkers();
 
     try {
       let str = JSON.stringify(canvassSettings);
@@ -540,6 +539,7 @@ export default class App extends PureComponent {
       } catch (e) {}
     }
 
+    if (this.state.canvassSettings.show_only_my_turf !== canvassSettings.how_only_my_turf) this.updateMarkers();
   }
 
   timeFormat(epoch) {
