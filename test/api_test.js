9d279d46439314bff85b8d6b5d133f6a271efb81
Corey Henderson <corman@cormander.com>
2018-06-23 17:02:24 -0600

    ES6 for tests

--- test/api_test.js
+++ test/api_test.js
@@ -22,250 +22,195 @@ let teamName = "I'm a little tea-pot";
 
 describe('API smoke', function () {
 
-  before(function (done) {
+  before(async () => {
+    let r;
 
     // clean up test data before we begin
     authToken = neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS);
     db = new BoltAdapter(neo4j.driver('bolt://'+process.env.NEO4J_HOST, authToken));
     db.cypherQueryAsync('match (a) detach delete a');
 
-    liveprd.post('/auth/jwt')
+    r = await liveprd.post('/auth/jwt')
       .set('Content-Type', 'application/json')
       .set('User-Agent', 'OurVoiceUSA/test')
       .send({
         apiKey: "12345678765432",
-      })
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        jwt_inval = res.body.jwt;
       });
+    expect(r.statusCode).to.equal(200);
+    jwt_inval = r.body.jwt;
 
-    livedev.post('/auth/jwt')
+    r = await livedev.post('/auth/jwt')
       .set('Content-Type', 'application/json')
       .set('User-Agent', 'OurVoiceUSA/test')
       .send({
         apiKey: "12345678765432",
-      })
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        jwt_bad = res.body.jwt;
       });
+    expect(r.statusCode).to.equal(200);
+    jwt_bad = r.body.jwt;
 
-    livedev.get('/auth/tokentest')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        jwt_admin = res.body.jwt;
-      });
+    r = await livedev.get('/auth/tokentest');
+    expect(r.statusCode).to.equal(200);
+    jwt_admin = r.body.jwt;
 
-    livedev.get('/auth/tokentest')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        jwt_bob = res.body.jwt;
-        done();
-      });
+    r = await livedev.get('/auth/tokentest');
+    expect(r.statusCode).to.equal(200);
+    jwt_bob = r.body.jwt;
 
   });
 
-  after(function (done) {
+  after(async () => {
     db.close();
-    done();
   });
 
-  it('poke 200 timestamp', function (done) {
-    api.get('/poke')
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        expect(res.body.data[0]).to.satisfy(Number.isInteger);
-        done();
-      });
+  it('poke 200 timestamp', async () => {
+    const r = await api.get('/poke');
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
+    expect(r.body.data[0]).to.satisfy(Number.isInteger);
   });
 
-  it('hello 400 no jwt', function (done) {
-    api.get('/canvass/v1/hello')
-      .expect(401)
-      .end(function (err, res) {
-        expect(res.body.error).to.equal(true);
-        expect(res.body.msg).to.equal("Missing required header.");
-        done();
-      });
+  it('hello 400 no jwt', async () => {
+    const r = await api.get('/canvass/v1/hello');
+    expect(r.statusCode).to.equal(400);
+    expect(r.body.error).to.equal(true);
+    expect(r.body.msg).to.equal("Missing required header.");
   });
 
-  it('hello 400 bad jwt', function (done) {
-    api.get('/canvass/v1/hello')
-      .set('Authorization', 'Bearer '+jwt_bad)
-      .expect(400)
-      .end(function (err, res) {
-        expect(res.body.error).to.equal(true);
-        expect(res.body.msg).to.equal("Your token is missing a required parameter.");
-        done();
-      });
+  it('hello 400 bad jwt', async () => {
+    const r = await api.get('/canvass/v1/hello')
+      .set('Authorization', 'Bearer '+jwt_bad);
+    expect(r.statusCode).to.equal(400);
+    expect(r.body.error).to.equal(true);
+    expect(r.body.msg).to.equal("Your token is missing a required parameter.");
   });
 
-  it('hello 403 invalid jwt', function (done) {
-    api.get('/canvass/v1/hello')
-      .set('Authorization', 'Bearer '+jwt_inval)
-      .expect(403)
-      .end(function (err, res) {
-        expect(res.body.error).to.equal(true);
-        expect(res.body).to.have.property("msg");
-        done();
-      });
+  it('hello 401 invalid jwt', async () => {
+    const r = await api.get('/canvass/v1/hello')
+      .set('Authorization', 'Bearer '+jwt_inval);
+    expect(r.statusCode).to.equal(401);
+    expect(r.body.error).to.equal(true);
+    expect(r.body).to.have.property("msg");
   });
 
-  it('hello 200 admin awaiting assignment', function (done) {
-    api.get('/canvass/v1/hello')
-      .set('Authorization', 'Bearer '+jwt_admin)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body).to.have.property("msg");
-        expect(res.body.msg).to.equal("Awaiting assignment");
-        expect(res.body.data).to.have.property("ready");
-        expect(res.body.data.ready).to.equal(false);
-        done();
-      });
+  it('hello 200 admin awaiting assignment', async () => {
+    const r = await api.get('/canvass/v1/hello')
+      .set('Authorization', 'Bearer '+jwt_admin);
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body).to.have.property("msg");
+    expect(r.body.msg).to.equal("Awaiting assignment");
+    expect(r.body.data).to.have.property("ready");
+    expect(r.body.data.ready).to.equal(false);
   });
 
-  it('hello 200 bob awaiting assignment', function (done) {
-    api.get('/canvass/v1/hello')
-      .set('Authorization', 'Bearer '+jwt_bob)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body).to.have.property("msg");
-        expect(res.body.msg).to.equal("Awaiting assignment");
-        expect(res.body.data).to.have.property("ready");
-        expect(res.body.data.ready).to.equal(false);
-        done();
-      });
+  it('hello 200 bob awaiting assignment', async () => {
+    const r = await api.get('/canvass/v1/hello')
+      .set('Authorization', 'Bearer '+jwt_bob);
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body).to.have.property("msg");
+    expect(r.body.msg).to.equal("Awaiting assignment");
+    expect(r.body.data).to.have.property("ready");
+    expect(r.body.data.ready).to.equal(false);
   });
 
   // TODO: check admin full list vs. non-admin only see your own team
 
-  it('canvasser/list 200 array', function (done) {
-    api.get('/canvass/v1/canvasser/list')
-      .set('Authorization', 'Bearer '+jwt_admin)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
-      });
+  it('canvasser/list 200 array', async () => {
+    const r = await api.get('/canvass/v1/canvasser/list')
+      .set('Authorization', 'Bearer '+jwt_admin);
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
   });
 
   // TODO: lock a user from above, make sure that user gets a 403, unlock it, then 200
 
-  it('canvasser/lock 200 array', function (done) {
-    api.post('/canvass/v1/canvasser/lock')
+  it('canvasser/lock 200 array', async () => {
+    const r = await api.post('/canvass/v1/canvasser/lock')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         id: "12345678765432",
-      })
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
       });
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
   });
 
-  it('canvasser/unlock 200 array', function (done) {
-    api.post('/canvass/v1/canvasser/unlock')
+  it('canvasser/unlock 200 array', async () => {
+    const r = await api.post('/canvass/v1/canvasser/unlock')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         id: "12345678765432",
-      })
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
       });
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
   });
 
   // TODO: check admin full list vs. non-admin only see your own teams
 
-  it('team/list 200 array', function (done) {
-    api.get('/canvass/v1/team/list')
-      .set('Authorization', 'Bearer '+jwt_admin)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
-      });
+  it('team/list 200 array', async () => {
+    const r = await api.get('/canvass/v1/team/list')
+      .set('Authorization', 'Bearer '+jwt_admin);
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
   });
 
-  it('team/create 400 invalid characters', function (done) {
-    api.post('/canvass/v1/team/create')
+  it('team/create 400 invalid characters', async () => {
+    const r = await api.post('/canvass/v1/team/create')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         name: "_",
-      })
-      .expect(400)
-      .end(function (err, res) {
-        expect(res.body).to.have.property("error");
-        done();
       });
+    expect(r.statusCode).to.equal(400);
+    expect(r.body).to.have.property("error");
   });
 
   // TODO: list to check that it's there, and again to see it missing
 
-  it('team/create team/delete', function (done) {
+  it('team/create team/delete', async () => {
+    let r;
 
-    api.post('/canvass/v1/team/create')
+    r = await api.post('/canvass/v1/team/create')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         name: teamName,
-      })
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
       });
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
 
-    api.post('/canvass/v1/team/create')
+    r = await api.post('/canvass/v1/team/create')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         name: teamName,
-      })
-      .expect(500)
-      .end(function (err, res) {
-        expect(res.body.error).to.equal(true);
-        expect(res.body.msg).to.be.an('string');
       });
+    expect(r.statusCode).to.equal(500);
+    expect(r.body.error).to.equal(true);
+    expect(r.body.msg).to.be.an('string');
 
-    api.post('/canvass/v1/team/delete')
+    r = await api.post('/canvass/v1/team/delete')
       .set('Authorization', 'Bearer '+jwt_admin)
       .send({
         name: teamName,
-      })
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
       });
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
 
   });
 
   // TODO: check admin full list vs. non-admin only see your own teams
 
-  it('team/member/list 200 array', function (done) {
-    api.get('/canvass/v1/team/members/list?teamName='+teamName)
-      .set('Authorization', 'Bearer '+jwt_admin)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.not.have.property("error");
-        expect(res.body.data).to.be.an('array');
-        done();
-      });
+  it('team/member/list 200 array', async () => {
+    const r = await api.get('/canvass/v1/team/members/list?teamName='+teamName)
+      .set('Authorization', 'Bearer '+jwt_admin);
+    expect(r.statusCode).to.equal(200);
+    expect(r.body).to.not.have.property("error");
+    expect(r.body.data).to.be.an('array');
   });
 
 });
