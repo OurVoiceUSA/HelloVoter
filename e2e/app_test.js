3744163dfdc2c204e098fd358d4aac4676935582
Corey Henderson <corman@cormander.com>
2018-06-26 22:46:19 -0600

    initial commit of detox / mocha test automation

---
+++ e2e/app_test.js
@@ -0,0 +1,26 @@
+
+const detox = require('detox');
+const config = require('../package.json').detox;
+
+before(async () => {
+  await detox.init(config);
+  await device.launchApp({permissions: {notifications: 'YES', location: 'inuse'}});
+});
+
+after(async () => {
+  await detox.cleanup();
+});
+
+describe('Example', () => {
+
+  it('should have about button', async () => {
+    await expect(element(by.text('About Our Voice'))).toBeVisible();
+  });
+  
+  it('navigate about our voice', async () => {
+    await element(by.text('About Our Voice')).tap();
+    await expect(element(by.text('Who We Are'))).toBeVisible();
+  });
+
+})
+
