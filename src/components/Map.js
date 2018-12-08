65332bc1cb904a9952929fed53d2cb0c35b9d40b
Corey Henderson <corman@cormander.com>
2018-12-08 12:42:55 -0700

    load google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 
+import GoogleMaps from 'google-map-react';
+
+import { google_api_key } from '../config.js';
+
 export default class App extends Component {
   render() {
-    return (<div>Map Component</div>);
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMaps
+          bootstrapURLKeys={{ key: google_api_key }}
+          defaultCenter={{lat: 33.9208231, lng: -118.3281370}}
+          defaultZoom={11}
+        >
+          <Pin
+            lat={59.955413}
+            lng={30.337844}
+          />
+        </GoogleMaps>
+      </div>
+    );
   }
 }
+
+const Pin = (props) => (
+  <div>
+    FOOBAR
+  </div>
+)
+
