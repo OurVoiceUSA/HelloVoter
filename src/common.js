1cffcde8c93b5f79bd92b226630c29e379e0a8f5
Corey Henderson <corman@cormander.com>
2018-12-11 05:02:26 -0700

    silence warning

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LoaderSpinner from 'react-loader-spinner';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
