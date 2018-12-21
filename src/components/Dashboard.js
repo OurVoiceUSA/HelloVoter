79af8452d16702c8f6735c3decf495a592cb9d2e
Corey Henderson <corman@cormander.com>
2018-12-20 22:18:34 -0700

    simple dashboard cards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import { _fetch, notify_error, RootLoader } from '../common.js';
+import { faShieldAlt, faUser, faUsers, faMap, faClipboard, faChartPie, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+import { _fetch, notify_error, RootLoader, Icon } from '../common.js';
 
 export default class App extends Component {
 
@@ -9,6 +11,7 @@ export default class App extends Component {
 
     this.state = {
       loading: true,
+      data: {},
     };
   }
 
@@ -35,8 +38,24 @@ export default class App extends Component {
   render() {
     return (
       <RootLoader flag={this.state.loading} func={this._loadData}>
-        {JSON.stringify(this.state.data)}
+        <CardDashboard name="Canvassers" stat={this.state.data.canvassers} icon={faUser} />
+        <CardDashboard name="Teams" stat={this.state.data.teams} icon={faUsers} />
+        <CardDashboard name="Turf" stat={this.state.data.turfs} icon={faMap} />
+        <CardDashboard name="Forms" stat={this.state.data.forms} icon={faClipboard} />
+        <CardDashboard name="Questions" stat={this.state.data.questions} icon={faChartPie} />
+        <CardDashboard name="Addresses" stat={this.state.data.addresses} icon={faMapMarkerAlt} />
       </RootLoader>
     );
   }
 }
+
+const CardDashboard = (props) => (
+  <div style={{display: 'flex', padding: '10px'}}>
+    <div style={{padding: '5px 10px'}}>
+      <Icon style={{width: 50, height: 50, color: "gray"}} icon={(props.icon?props.icon:faShieldAlt)} />
+    </div>
+    <div style={{flex: 1, overflow: 'auto'}}>
+      <h3>{props.name}: {props.stat}</h3>
+    </div>
+  </div>
+)
