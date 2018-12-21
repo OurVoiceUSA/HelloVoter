a12b5c45c7fc38d96042e9c4aa35dbe6e336b827
Corey Henderson <corman@cormander.com>
2018-12-20 23:09:41 -0700

    same

--- src/components/Canvassers.js
+++ src/components/Canvassers.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import { RootLoader, CardCanvasser, _loadCanvassers, _searchStringCanvasser } from '../common.js';
 
@@ -32,50 +32,54 @@ export default class App extends Component {
 
   render() {
 
-    let denied = [];
     let ready = [];
     let unassigned = [];
+    let denied = [];
 
     this.state.canvassers.forEach(c => {
       if (this.state.search && !_searchStringCanvasser(c).includes(this.state.search)) return;
       if (c.locked) {
-        denied.push(<CardCanvasser key={c.id} canvasser={c} refer={this} />)
+        denied.push(c);
       } else {
         if (c.ass.ready || c.ass.teams.length)
-          ready.push(<CardCanvasser key={c.id} canvasser={c} refer={this} />)
+          ready.push(c);
         else
-          unassigned.push(<CardCanvasser key={c.id} canvasser={c} refer={this} />);
+          unassigned.push(c);
       }
     });
 
     return (
-      <Router>
-        <div>
-          <Route exact={true} path="/canvassers/" render={() => (
-            <RootLoader flag={this.state.loading} func={() => this._loadData()}>
-              Search: <input type="text" value={this.state.value} onChange={this.onTypeSearch} data-tip="Search by name, email, location, or admin" />
-              <br />
-              {unassigned.length?
-              <div>
-                <h3>Unassigned Canvassers ({unassigned.length})</h3>
-                {unassigned}
-              </div>
-              :''}
-              <h3>Canvassers ({ready.length})</h3>
-              {ready}
-              {denied.length?
-              <div>
-                <h3>Denied access ({denied.length})</h3>
-                {denied}
-              </div>
-              :''}
-            </RootLoader>
-          )} />
-          <Route path="/canvassers/:id" render={(props) => (
-            <CardCanvasser key={props.match.params.id} id={props.match.params.id} edit={true} refer={this} />
-          )} />
-        </div>
-      </Router>
+      <RootLoader flag={this.state.loading} func={() => this._loadData()}>
+
+        Search: <input type="text" value={this.state.value} onChange={this.onTypeSearch} data-tip="Search by name, email, location, or admin" />
+        <br />
+        <Link to={'/canvassers/'}>Canvassers</Link> - <Link to={'/canvassers/unassigned'}>Unassigned</Link> - <Link to={'/canvassers/denied'}>Denied</Link>
+        <Router>
+          <div>
+            <Route exact={true} path="/canvassers/" render={() => (<ListCanvassers refer={this} canvassers={ready} />)} />
+            <Route exact={true} path="/canvassers/unassigned" render={() => (<ListCanvassers refer={this} type="Unassigned" canvassers={unassigned} />)} />
+            <Route exact={true} path="/canvassers/denied" render={() => (<ListCanvassers refer={this} type="Denied" canvassers={denied} />)} />
+            <Route path="/canvassers/view/:id" render={(props) => (
+              <CardCanvasser key={props.match.params.id} id={props.match.params.id} edit={true} refer={this} />
+            )} />
+          </div>
+        </Router>
+      </RootLoader>
     );
   }
 }
+
+const ListCanvassers = (props) => {
+  let list = [];
+
+  props.canvassers.forEach((c, idx) => {
+    list.push(<CardCanvasser key={c.id} canvasser={c} refer={props.refer} />);
+  });
+
+  return (
+    <div>
+      <h3>{props.type}Canvassers ({props.canvassers.length})</h3>
+      {list}
+    </div>
+  );
+};
