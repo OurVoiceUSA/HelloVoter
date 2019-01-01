d4cd473469f420d3c5dcbe19e851ea08bb2139d5
Corey Henderson <corman@cormander.com>
2018-12-31 19:10:51 -0700

    fix live login

--- src/components/Jwt.js
+++ src/components/Jwt.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { Route, Redirect } from 'react-router'
-import jwt_decode from 'jwt-decode';
+import jwt from 'jsonwebtoken';
 import {notify_error} from '../common.js';
 
 export default class App extends Component {
@@ -9,22 +9,22 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    let jwt;
+    let token;
 
     try {
-      jwt = this.props.location.pathname.split('/').pop();
-      jwt_decode(jwt);
-      this.props.refer._loadData(jwt);
+      token = this.props.location.pathname.split('/').pop();
+      jwt.decode(token);
+      this.props.refer._loadData(token);
     } catch (e) {
       notify_error(e, "Unable to extract jwt from URI");
-      jwt = 'error';
+      token = 'error';
     }
 
-    this.state = {jwt: jwt};
+    this.state = {token: token};
   }
 
   render() {
-    if (this.state.jwt === 'error') return (<div>There was an error parsing the jwt</div>);
+    if (this.state.token === 'error') return (<div>There was an error parsing the jwt</div>);
 
     return (
       <Route render={() => (
