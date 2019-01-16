4a269923b87aa6c2082c47005b53c0181cc33245
matthewjhough <matthough1@icloud.com>
2019-01-16 11:02:44 -0500

    changed duplicate mock names & update references

--- src/components/importData/__mocks__/index.js
+++ src/components/importData/__mocks__/index.js
@@ -1,4 +1,4 @@
-import { testHeaders1, testBody1, formatObject1 } from './excel1';
-import { findInnerElement } from './utilities';
+import { testHeaders1, testBody1, formatObject1 } from "./excel1";
+import { findInnerElement } from "./import_data_utilities";
 
 export { testHeaders1, testBody1, formatObject1, findInnerElement };
