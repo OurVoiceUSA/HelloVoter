52255eb33d9d4bb4619c0f96b43abae27e848378
Corey Henderson <corman@cormander.com>
2019-01-21 11:21:13 -0700

    Page to view job queue status and history

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Analytics from './Analytics';
 import Dashboard from './Dashboard';
 import Forms from './Forms';
 import { ImportData } from './importData/index';
+import Queue from './Queue';
 import Jwt from './Jwt';
 import Login from './Login';
 import LogoutDialog from './LogoutDialog';
@@ -19,6 +20,7 @@ export {
   Dashboard,
   Forms,
   ImportData,
+  Queue,
   Jwt,
   Login,
   LogoutDialog,
