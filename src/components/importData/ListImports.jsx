66ece79c9ad6eb6e03ffe683e1627fd0383a28b9
Corey Henderson <corman@cormander.com>
2019-01-20 17:23:39 -0700

    initial commit of table for import list

--- src/components/importData/ListImports.jsx
+++ src/components/importData/ListImports.jsx
@@ -1,76 +1,212 @@
-import React from 'react';
-import ReactPaginate from 'react-paginate';
-import Select from 'react-select';
-import List from '@material-ui/core/List';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
-const CardImport = props => {
-  return (
-    <div>
-      {JSON.stringify(props.import)}
-      <br />
-      <br />
-      <br />
-    </div>
-  );
-};
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
 
-const ListImports = ({
-  perPage,
-  imports,
-  pageNum,
-  handlePageClick,
-  handlePageNumChange
-}) => {
-  let paginate = <div />;
-  let list = [];
+const jobRuntime = (start, end) => {
+  if (end)
+    return Math.ceil((end-start)/1000);
+  else
+    return "";
+}
 
-  imports.forEach((i, idx) => {
-    let tp = Math.floor(idx / perPage) + 1;
-    if (tp !== pageNum) return;
-    list.push(<CardImport key={i.filename} import={i} />);
-  });
+class TablePaginationActions extends Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
 
-  paginate = (
-    <div style={{ display: 'flex' }}>
-      <ReactPaginate
-        previousLabel={'previous'}
-        nextLabel={'next'}
-        breakLabel={'...'}
-        breakClassName={'break-me'}
-        pageCount={imports.length / perPage}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={8}
-        onPageChange={handlePageClick}
-        containerClassName={'pagination'}
-        subContainerClassName={'pages pagination'}
-        activeClassName={'active'}
-      />
-      &nbsp;&nbsp;&nbsp;
-      <div style={{ width: 75 }}>
-        # Per Page{' '}
-        <Select
-          value={{ value: perPage, label: perPage }}
-          onChange={handlePageNumChange}
-          options={[
-            { value: 5, label: 5 },
-            { value: 10, label: 10 },
-            { value: 25, label: 25 },
-            { value: 50, label: 50 },
-            { value: 100, label: 100 }
-          ]}
-        />
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
       </div>
-    </div>
-  );
+    );
+  }
+}
 
-  return (
-    <div>
-      <h3>Import History</h3>
-      {paginate}
-      <List component="nav">{list}</List>
-      {paginate}
-    </div>
-  );
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default ListImports;
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+  TablePaginationActions,
+);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 500,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+});
+
+class ListImports extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let token;
+
+    this.state = {
+      rows: this.props.imports,
+      page: 0, // ?? this.props.pageNum,
+      rowsPerPage: 5, // ?? this.props.perPage,
+    };
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { rows, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    /*
+    "created"
+    "submitted"
+    "parse_start"
+    "num_records"
+    "num_people"
+    "num_addresses"
+    "geocode_start"
+    "geocode_success"
+    "goecode_fail"
+    "dedupe_start"
+    "dupes_address"
+    "index_start"
+    "turfadd_start"
+    "completed"
+    */
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table}>
+            <TableBody>
+              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.filename}
+                  </TableCell>
+                  <TableCell align="right">{jobRuntime(row.parse_start, row.parse_end)}</TableCell>
+                  <TableCell align="right">{jobRuntime(row.geocode_start, row.geocode_end)}</TableCell>
+                  <TableCell align="right">{jobRuntime(row.dedupe_start, row.dedupe_end)}</TableCell>
+                  <TableCell align="right">{jobRuntime(row.index_start, row.index_end)}</TableCell>
+                  <TableCell align="right">{jobRuntime(row.turfadd_start, row.turfadd_end)}</TableCell>
+                  <TableCell align="right">{jobRuntime(row.parse_start, row.completed)}</TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    native: true,
+                  }}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActionsWrapped}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </Paper>
+    );
+  }
+}
+
+ListImports.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ListImports);
