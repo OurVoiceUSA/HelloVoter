4ba5d2f969d34c76c2813f7aa72e3af1ac97911c
Larry Person <lp10011@gmail.com>
2019-01-21 20:58:49 -0500

    dashboard cards are sortable in a single column

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import NumberFormat from 'react-number-format';
 import filesize from 'filesize';
 
-import { faShieldAlt, faUser, faUsers, faMap, faClipboard, faChartPie, faMapMarkerAlt, faDatabase } from '@fortawesome/free-solid-svg-icons';
+import {
+  faShieldAlt,
+  faUser,
+  faUsers,
+  faMap,
+  faClipboard,
+  faChartPie,
+  faMapMarkerAlt,
+  faDatabase,
+} from '@fortawesome/free-solid-svg-icons';
+import { arrayMove, sortableContainer, sortableElement } from 'react-sortable-hoc';
 
 import { _fetch, notify_error, RootLoader, Icon } from '../common.js';
 
 export default class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       loading: true,
       data: {},
+      cards: [],
     };
   }
 
@@ -21,42 +31,119 @@ export default class App extends Component {
     this._loadData();
   }
 
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(() => ({
+      cards: arrayMove(this.state.cards, oldIndex, newIndex),
+    }));
+  }
+
   _loadData = async () => {
     let data = {};
+    let cards = [];
 
-    this.setState({loading: true})
+    this.setState({ loading: true });
 
     try {
       data = await _fetch(this.props.server, '/volunteer/v1/dashboard');
+      cards = [
+        {
+          name: 'Volunteers',
+          stat: data.volunteers,
+          icon: faUser,
+        },
+        {
+          name: 'Teams',
+          stat: data.teams,
+          icon: faUsers,
+        },
+        {
+          name: 'Turfs',
+          stat: data.turfs,
+          icon: faMap,
+        },
+        {
+          name: 'Forms',
+          stat: data.forms,
+          icon: faClipboard,
+        },
+        {
+          name: 'Questions',
+          stat: data.questions,
+          icon: faChartPie,
+        },
+        {
+          name: 'Addresses',
+          stat: 
+          (
+            <NumberFormat
+              value={data.addresses}
+              displayType={'text'}
+              thousandSeparator={true}
+            />
+          ),
+          icon: faMapMarkerAlt,
+        },
+        {
+          name: 'Database size',
+          stat: filesize(data.dbsize ? data.dbsize : 0, {
+            round: 1,
+          }),
+          icon: faDatabase,
+        },
+      ];
+
     } catch (e) {
-      notify_error(e, "Unable to load dashboard info.");
+      notify_error(e, 'Unable to load dashboard info.');
     }
 
-    this.setState({data: data, loading: false});
+    this.setState({ cards, data, loading: false });
   }
 
   render() {
     return (
       <RootLoader flag={this.state.loading} func={this._loadData}>
-        <CardDashboard name="Volunteers" stat={this.state.data.volunteers} icon={faUser} />
-        <CardDashboard name="Teams" stat={this.state.data.teams} icon={faUsers} />
-        <CardDashboard name="Turfs" stat={this.state.data.turfs} icon={faMap} />
-        <CardDashboard name="Forms" stat={this.state.data.forms} icon={faClipboard} />
-        <CardDashboard name="Questions" stat={this.state.data.questions} icon={faChartPie} />
-        <CardDashboard name="Addresses" stat={(<NumberFormat value={this.state.data.addresses} displayType={'text'} thousandSeparator={true} />)} icon={faMapMarkerAlt} />
-        <CardDashboard name="Database size" stat={filesize((this.state.data.dbsize?this.state.data.dbsize:0), {round: 1})} icon={faDatabase} />
+        <Cards
+          state={this.state}
+          axis="xy"
+          onSortEnd={this.onSortEnd}
+          cards={this.state.cards}
+        />
       </RootLoader>
     );
   }
 }
 
-const CardDashboard = (props) => (
-  <div style={{display: 'flex', padding: '10px'}}>
-    <div style={{padding: '5px 10px'}}>
-      <Icon style={{width: 50, height: 50, color: "gray"}} icon={(props.icon?props.icon:faShieldAlt)} />
+const _Cards = props => {
+  return <div>
+    {
+      props.cards.map((card, index) => 
+        <CardDashboard
+          name={card.name}
+          stat={card.stat}
+          icon={card.icon}
+          index={index}
+        />
+      )
+    }
+  </div>;
+};
+
+const Cards = sortableContainer(_Cards);
+
+const _CardDashboard = props => (
+  <div style={{ display: 'flex', padding: '10px' }}>
+    <div style={{ padding: '5px 10px' }}>
+      <Icon
+        style={{ width: 50, height: 50, color: 'gray' }}
+        icon={props.icon ? props.icon : faShieldAlt}
+      />
     </div>
-    <div style={{flex: 1, overflow: 'auto'}}>
-      <h3>{props.name}: {props.stat}</h3>
+    <div style={{ flex: 1, overflow: 'auto' }}>
+      <h3>
+        {props.name}: {props.stat}
+      </h3>
     </div>
   </div>
-)
+);
+
+const CardDashboard = sortableElement(_CardDashboard);
