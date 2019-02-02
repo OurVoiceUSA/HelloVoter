987d4007727634363d915a9c500e6c9496bfe822
Corey Henderson <corman@cormander.com>
2019-02-02 12:11:38 -0700

    select turf to narrow party breakdown

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
+import Select from 'react-select';
 import Checkbox from '@material-ui/core/Checkbox';
 
 import {PieChart, Pie, Cell, Legend, Label} from 'recharts';
 
 import {
   _fetch,
+  _loadTurfs,
+  _searchStringify,
   RootLoader,
 } from '../common.js';
+
+import { CardTurf } from './Turf';
 
 const renderLabelContent = (props) => {
   const { value, percent, x, y, midAngle } = props;
@@ -26,20 +31,44 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      turfOptions: [],
+      selectedTurfOption: null,
       party_breakdown: [],
       animation: true,
     };
   }
 
   componentDidMount() {
-    this._loadData();
+    this._init();
+  }
+
+  _init = async () => {
+    this.setState({ loading: true });
+
+    let turfOptions = [];
+    let turfs = await _loadTurfs(this);
+
+    turfs.forEach(t => {
+      turfOptions.push({
+        value: _searchStringify(t),
+        id: t.id,
+        label: <CardTurf key={t.id} turf={t} refer={this} />,
+      });
+    });
+
+    this.setState({turfOptions}, () => this._loadData());
   }
 
   _loadData = async () => {
     this.setState({ loading: true });
 
     let party_breakdown = [];
-    let data = await _fetch(this.props.server, '/volunteer/v1/analytics/list'+(this.state.include_null?'?include_null=true':''));
+
+    let uri = '/volunteer/v1/analytics/list?turfId=';
+    if (this.state.selectedTurfOption && this.state.selectedTurfOption.id) uri += this.state.selectedTurfOption.id;
+    if (this.state.include_null) uri += '&include_null=true';
+
+    let data = await _fetch(this.props.server, uri);
 
     if (data && data.data) {
       data.data.map(d => party_breakdown.push({name: (d[0]?d[0]:'No Data'), value: d[1]}));
@@ -48,13 +77,24 @@ export default class App extends Component {
     this.setState({ party_breakdown, loading: false });
   }
 
+  handleTurfChange = selectedTurfOption => this.setState({selectedTurfOption}, () => this._loadData());
+
   render() {
     return (
       <RootLoader flag={this.state.loading} func={() => this._loadData()}>
         <h3>Analytics</h3>
+        <Select
+          value={this.state.selectedTurfOption}
+          onChange={this.handleTurfChange}
+          options={this.state.turfOptions}
+          isMulti={false}
+          isSearchable={true}
+          placeholder="Select a turf to include only records within that turf"
+        />
         <Checkbox color="primary" checked={this.state.include_null} onChange={(e, c) => {
           this.setState({include_null: c}, async () => await this._loadData());
         }} /> Include records with "No Data"
+        {this.state.party_breakdown.length?
         <PieChart width={800} height={400}>
           <Legend />
           <Pie
@@ -78,6 +118,9 @@ export default class App extends Component {
             </Label>
           </Pie>
         </PieChart>
+        :
+        <h3>No Data</h3>
+        }
       </RootLoader>
     );
   }
