220dde478b3772338a8199db858d6583d7600579
Corey Henderson <corman@cormander.com>
2019-02-11 20:54:08 -0700

    update logo on homescreen

--- src/components/HomeScreenPage/index.js
+++ src/components/HomeScreenPage/index.js
@@ -38,7 +38,7 @@ export default class App extends PureComponent {
     return (
       <View style={{flex: 1, backgroundColor: 'white'}}>
 
-        <Image source={homeImage} style={{padding: 15, alignSelf: 'center', maxWidth: Dimensions.get('window').width}} resizeMode={'contain'} />
+        <Image source={homeImage} style={{margin: 25, padding: 15, alignSelf: 'center', maxWidth: Dimensions.get('window').width}} resizeMode={'contain'} />
 
         <View style={{flex: 1, alignItems: 'center'}}>
 
