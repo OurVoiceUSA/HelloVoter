9207d65f96a2653f4aeeaab0fec1c85bccd0ba11
Corey Henderson <corman@cormander.com>
2019-06-13 17:59:13 -0600

    move express startup to server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 
-import { doExpressStartup } from './lib/express';
+import https from 'https';
+import fs from 'fs';
+
 import { doStartupTasks } from './lib/startup';
+import { doExpressInit } from './lib/express';
 import { ov_config } from './lib/ov_config';
 import neo4j from './lib/neo4j';
 import queue from './lib/queue';
 
 const db = new neo4j(ov_config);
-
 const qq = new queue(db);
 
 db.query('return timestamp()')
@@ -15,5 +17,23 @@ db.query('return timestamp()')
     process.exit(1)}
   ).then(async () => {
     await doStartupTasks(db, qq);
-    doExpressStartup(db, qq);
+
+    const app = doExpressInit(true, db, qq);
+
+    // Launch the server
+    if (ov_config.server_ssl_port && ov_config.server_ssl_key && ov_config.server_ssl_cert) {
+      console.log('express.js SSL startup');
+      https.createServer(
+        {
+          key: fs.readFileSync(ov_config.server_ssl_key),
+          cert: fs.readFileSync(ov_config.server_ssl_cert),
+        }, app
+      ).listen(ov_config.server_ssl_port);
+    } else {
+      const server = app.listen(ov_config.server_port, () => {
+        const { address, port } = server.address();
+        console.log('express.js startup');
+        console.log(`Listening at http://${address}:${port}`);
+      });
+    }
   });
