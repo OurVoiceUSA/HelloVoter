3b6cd97b52eaed026bb97cf4e6fd9dcc9f19619c
Corey Henderson <corman@cormander.com>
2019-06-18 08:59:36 -0600

    initial commit of zero visibility functional test

---
+++ test/functional/new-vol-zero-visibility.js
@@ -0,0 +1,64 @@
+
+import { expect } from 'chai';
+
+import { ov_config } from '../../lib/ov_config';
+import neo4j from '../../lib/neo4j';
+import { appInit, base_uri, getObjs, sm_oauth } from '../lib/utils';
+
+var api;
+var db;
+var c, teams, turfs, forms;
+
+describe('New Volunteer Zero Visibility', function () {
+
+  before(() => {
+    db = new neo4j(ov_config);
+    api = appInit(db);
+    c = getObjs('volunteers');
+    teams = getObjs('teams');
+    turfs = getObjs('turfs');
+    forms = getObjs('forms');
+  });
+
+  after(async () => {
+    db.close();
+  });
+
+  // TODO: need to setup some assignments for these tests to be fully valid
+  // TODO: need to hit more endpoints
+
+  it('can see only self', async () => {
+    let r = await api.get(base_uri+'/volunteer/list')
+      .set('Authorization', 'Bearer '+c.mike.jwt)
+    expect(r.statusCode).to.equal(200);
+    expect(r.body.length).to.equal(1);
+  });
+
+  it('can not list teams', async () => {
+    let r = await api.get(base_uri+'/team/list')
+      .set('Authorization', 'Bearer '+c.mike.jwt)
+    expect(r.statusCode).to.equal(200);
+    expect(r.body.data.length).to.equal(0);
+  });
+
+  it('can not list team members', async () => {
+    let r = await api.get(base_uri+'/team/members/list?teamId='+teams.A.id)
+      .set('Authorization', 'Bearer '+c.mike.jwt)
+    expect(r.statusCode).to.equal(200);
+    expect(r.body.length).to.equal(0);
+  });
+
+  it('can not list turfs', async () => {
+    let r = await api.get(base_uri+'/turf/list')
+      .set('Authorization', 'Bearer '+c.mike.jwt)
+    expect(r.statusCode).to.equal(200);
+    expect(r.body.data.length).to.equal(0);
+  });
+
+  it('can not list forms', async () => {
+    let r = await api.get(base_uri+'/form/list')
+      .set('Authorization', 'Bearer '+c.mike.jwt)
+    expect(r.statusCode).to.equal(200);
+    expect(r.body.data.length).to.equal(0);
+  });
+});
