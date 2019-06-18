d7a4c4b13d791ba9ec23f815221999db7dbbd0a7
Corey Henderson <corman@cormander.com>
2019-06-18 13:49:57 -0600

    move create to top and delete to bottom

--- app/routes/api/v1/attribute.js
+++ app/routes/api/v1/attribute.js
@@ -7,19 +7,6 @@ import {
 const Router = require('express').Router
 
 module.exports = Router({mergeParams: true})
-.get('/attribute/list', (req, res) => {
-  if (req.user.admin === true)
-    return cqdo(req, res, 'match (a:Attribute) return a order by a.order', {}, true);
-  else
-    return cqdo(req, res, `
-  match (v:Volunteer {id:{id}})
-  optional match (v)-[:ASSIGNED]-(f:Form) with v, collect(f) as dforms
-  optional match (v)-[:MEMBERS]-(:Team)-[:ASSIGNED]-(f:Form) with v, dforms + collect(f) as forms
-  unwind forms as f
-  match (a:Attribute)-[:COMPILED_ON]->(f)
-  return a order by a.order
-    `, req.user);
-})
 .post('/attribute/create', (req, res) => {
   if (!valid(req.body.name) || !valid(req.body.type)) return _400(res, "Invalid value to parameter 'name' or 'type'.");
   req.body.author_id = req.user.id;
@@ -37,9 +24,18 @@ module.exports = Router({mergeParams: true})
 
   return cqdo(req, res, 'match (a:Volunteer {id:{author_id}}) create (b:Attribute {id:randomUUID(), created: timestamp(), name:{name}, type:{type}})-[:AUTHOR]->(a)', req.body, true);
 })
-.post('/attribute/delete', (req, res) => {
-  if (!valid(req.body.id)) return _400(res, "Invalid value to parameter 'id'.");
-  return cqdo(req, res, 'match (a:Attribute {id:{id}}) detach delete a', req.body, true);
+.get('/attribute/list', (req, res) => {
+  if (req.user.admin === true)
+    return cqdo(req, res, 'match (a:Attribute) return a order by a.order', {}, true);
+  else
+    return cqdo(req, res, `
+  match (v:Volunteer {id:{id}})
+  optional match (v)-[:ASSIGNED]-(f:Form) with v, collect(f) as dforms
+  optional match (v)-[:MEMBERS]-(:Team)-[:ASSIGNED]-(f:Form) with v, dforms + collect(f) as forms
+  unwind forms as f
+  match (a:Attribute)-[:COMPILED_ON]->(f)
+  return a order by a.order
+    `, req.user);
 })
 .get('/attribute/form/list', (req, res) => {
   if (!valid(req.query.id)) return _400(res, "Invalid value to parameter 'id'.");
@@ -52,4 +48,8 @@ module.exports = Router({mergeParams: true})
 .post('/attribute/form/remove', (req, res) => {
   if (!valid(req.body.id) || !valid(req.body.formId)) return _400(res, "Invalid value to parameter 'key' or 'formId'.");
   return cqdo(req, res, 'match (a:Attribute {id:{id}})-[r:COMPILED_ON]-(b:Form {id:{formId}}) delete r', req.body, true);
+})
+.post('/attribute/delete', (req, res) => {
+  if (!valid(req.body.id)) return _400(res, "Invalid value to parameter 'id'.");
+  return cqdo(req, res, 'match (a:Attribute {id:{id}}) detach delete a', req.body, true);
 });
