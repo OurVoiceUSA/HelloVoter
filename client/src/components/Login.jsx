5ae32d826afa3dd1c2f2b7ddfad21bef8b48f408
Corey Henderson <corman@cormander.com>
2019-06-20 20:03:23 -0600

    remove unused reference

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Select from 'react-select';
-
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
