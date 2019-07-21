01fe664ef9fb4628a18c69879ff8f3eb1db4bafc
Corey Henderson <corman@cormander.com>
2019-07-21 15:07:45 -0600

    allow for a "not" operator flag

--- server/app/routes/api/v1/turf.js
+++ server/app/routes/api/v1/turf.js
@@ -12,8 +12,8 @@ async function stats_by_attr(req, aq) {
   let c = '';
   let ref;
   if (aq) {
-    ref = await req.db.query('match (aq:AttributeQuery {id:{id}})-[r:CONSTRAIN]->(at:Attribute) return at.id, r.op, r.value', {id: aq});
-    c = ref.data.map((attr, idx) => 'match (p)<-[:ATTRIBUTE_OF]-(pa'+idx+':PersonAttribute)-[:ATTRIBUTE_TYPE]->(:Attribute {id:"'+attr[0]+'"}) where pa'+idx+'.value '+attr[1]+' "'+attr[2]+'"').join(' ');
+    ref = await req.db.query('match (aq:AttributeQuery {id:{id}})-[r:CONSTRAIN]->(at:Attribute) return at.id, r.not, r.op, r.value', {id: aq});
+    c = ref.data.map((attr, idx) => 'match (p)<-[:ATTRIBUTE_OF]-(pa'+idx+':PersonAttribute)-[:ATTRIBUTE_TYPE]->(:Attribute {id:"'+attr[0]+'"}) where '+(attr[1]?'NOT':'')+' pa'+idx+'.value '+attr[2]+' "'+attr[3]+'"').join(' ');
   }
   ref = await req.db.query(`
 match (p:Person)-[:RESIDENCE {current:true}]->()-[*0..1]-(a:Address)-[:WITHIN]->(t:Turf {id:{turfId}})
