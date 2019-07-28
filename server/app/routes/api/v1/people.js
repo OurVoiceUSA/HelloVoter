21758d0dc67cf7b1f53ff97dcad1964d262192ff
Corey Henderson <corman@cormander.com>
2019-07-28 08:16:38 -0600

    remove autoturf and admin loosey goosey

--- server/app/routes/api/v1/people.js
+++ server/app/routes/api/v1/people.js
@@ -60,8 +60,6 @@ module.exports = Router({mergeParams: true})
   // make sure formId is in ass.forms
   if (ass.forms.map(f => f.id).indexOf(req.body.formId) === -1) return _403(res, "You are not assigned this form.");
 
-  req.body.autoturf_dist = 1000;
-
   try {
     req.body.id = req.user.id;
 
@@ -82,7 +80,7 @@ TODO: constrain update to a turf their assigned to, but without creating multipl
     with v, collect(t.id) as tt
   optional match (t:Turf)-[:ASSIGNED]->(v)
     with v, tt + collect(t.id) as turfIds
-`+((req.user.admin||req.user.autoturf)?'optional ':'')+`match (t:Turf) where t.id in turfIds
+  match (t:Turf) where t.id in turfIds
     with v
   match `+(req.body.personId?'(p:Person {id:{personId}})-[r:RESIDENCE {current:true}]->':'')+(req.body.unit?'(u:Unit {name:{unit}})-[:AT]->':'')+`(a:Address {id:{addressId}})-[:WITHIN]->(t)
 
@@ -92,10 +90,6 @@ TODO: constrain update to a turf their assigned to, but without creating multipl
     ref = await req.db.query(`
   match (v:Volunteer {id:{id}})
   match `+(req.body.personId?'(p:Person {id:{personId}})-[r:RESIDENCE {current:true}]->':'')+(req.body.unit?'(u:Unit {name:{unit}})-[:AT]->':'')+`(a:Address {id:{addressId}})
-`+((!req.user.admin&&req.user.autoturf)?`
-    using index a:Address(id)
-      where distance(a.position, v.location) < {autoturf_dist}
-`:'')+`
   match (d:Device {UniqueID:{deviceId}})-[:USED_BY]->(v),
     (f:Form {id:{formId}})
   create (vi:Visit {
@@ -139,17 +133,20 @@ TODO: constrain update to a turf their assigned to, but without creating multipl
 async function visitsAndPeople(req, res) {
   let ref = {};
 
+  if (!req.query.formId) return _400(res, "Invalid value to parameter 'formId'.");
+
   if (req.query.limit) req.query.limit = parseInt(req.query.limit);
   if (req.query.dist) req.query.dist = parseInt(req.query.dist);
 
   req.query.id = req.user.id;
   req.query.visit_status = [0,1,2,3];
 
-  let empty_addrs = (req.user.admin?true:false);
+  // TODO: set to false if the canvassing mode is avoid places we've been
+  let empty_addrs = true;
 
   // no value? no key
   if (!req.query.filter_val) req.query.filter_key = null;
-  // even if admin, a filter removes this
+  // even if empty_addrs, a filter removes this
   if (req.query.filter_key) empty_addrs = false;
   // type convert value if needed
   switch (req.query.filter_val) {
@@ -158,64 +155,48 @@ async function visitsAndPeople(req, res) {
     default: break;
   }
 
-  // TODO: we need an isLeader ... where admin is a leader too
-
-  // non-admin limits
-  if (!req.user.admin) {
-    // non-admin requires formId so they can't see what's already been interacted with on this form
-    if (!req.query.formId) return _400(res, "Invalid value to parameter 'formId'.");
 /*
-    // TODO: system settings and team/individual permissions for dist/limit, etc by regular volunteer
+    // TODO: system settings and team/individual permissions for dist/limit, etc by volunteer
     req.query.limit = 25; // TODO: server setting for this?
     req.query.dist = 1000; // TODO: this too?
     req.query.visit_status = [0,2]; // pick up 'not interested' here so we can filter the address/unit entirely
 */
-  }
-
   if (!req.query.limit) req.query.limit = 1000;
   if (!req.query.dist) req.query.dist = 10000;
-
-  // poll assignments to get autoturf
-  await volunteerAssignments(req, req.user);
-
-  req.query.autoturf_dist = 1000;
 
   try {
     // in rural areas this query can return zero -- retry with an order of magnatude incrasea ... twice if we have to
     let retry = 0;
-    // don't retry on autoturf
-    if (!empty_addrs && req.user.autoturf) retry = 2;
 
     while (retry <= 2) {
       if (retry && req.query.dist <= 100000) req.query.dist *= 10;
 
-      let q = `match (v:Volunteer {id:{id}}) `;
-
-      // non-admins are constrained to turf
-      if (!req.user.admin) q += `
+      let q = `match (v:Volunteer {id:{id}})
   optional match (t:Turf)-[:ASSIGNED]->(:Team)-[:MEMBERS]->(v)
     with v, collect(t.id) as tt
   optional match (t:Turf)-[:ASSIGNED]->(v)
-    with v, tt + collect(t.id) as turfIds
-  `+(req.user.autoturf?`optional `:``)+`match (t:Turf) where t.id in turfIds
-    with v, t, turfIds `;
+    with tt + collect(t.id) as turfIds
+  match (t:Turf) where t.id in turfIds
+    with t `;
 
       // either target an address, or use the address index
       if (req.query.aId) q += `match (a:Address {id:{aId}}) `;
       else q += `match (a:Address) using index a:Address(position) `;
 
-      if (!req.user.admin||(req.user.admin&&!req.query.aId)) q += `where `;
+      q += `where (a)-[:WITHIN]->(t) `;
 
-      if (!req.user.admin) q += `(a)-[:WITHIN]->(t) `;
-      if (!req.user.admin && req.user.autoturf) q += `or distance(a.position, v.location) < {autoturf_dist} `;
-
-      if (!req.query.aId) q += (req.user.admin?``:`and `)+`distance(a.position, point({longitude: {longitude}, latitude: {latitude}})) < {dist}
+      if (!req.query.aId) q += `and distance(a.position, point({longitude: {longitude}, latitude: {latitude}})) < {dist}
     with a, distance(a.position, point({longitude: {longitude}, latitude: {latitude}})) as dist
     order by dist limit {limit} `;
 
-      q += `optional match (u:Unit)-[:AT]->(a) with a, u optional match (person:Person)-[:RESIDENCE {current:true}]->(u) `;
+      q += `with distinct(a) as a
+    optional match (u:Unit)-[:AT]->(a)
+      with a, u
+    optional match (person:Person)-[:RESIDENCE {current:true}]->(u) `;
 
-      if (req.query.filter_key) q += `where ((u)<-[:RESIDENCE {current:true}]-(:Person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}}) or (person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})) `;
+      if (req.query.filter_key) q += `
+        where ((u)<-[:RESIDENCE {current:true}]-(:Person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})
+        or (person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})) `;
 
       if (req.query.filter_visited) q += (req.query.filter_key?`and`:`where`)+` not (person)<-[:VISIT_PERSON]-(:Visit)-[:VISIT_FORM]->(:Form {id:{formId}}) `;
 
@@ -223,29 +204,31 @@ async function visitsAndPeople(req, res) {
     with a, u, person, collect({id:attr.id, name:attr.name, value:pattr.value}) as attrs
     with a, u, collect(person{.*, attrs:attrs}) as people `;
 
-      if (!empty_addrs&&req.query.formId) q += `where size(people) > 0 or u is null `;
-
-      if (req.query.formId) q += `optional match (u)<-[:VISIT_AT]-(v:Visit)-[:VISIT_FORM]->(:Form {id:{formId}}) where v.status in {visit_status} with a, u, people, collect(v) as visits `+(req.user.admin?``:`, collect(v.status) as status where not 2 in status or status is null `);
+      if (!empty_addrs) q += `where size(people) > 0 or u is null `;
 
       q += `
+  optional match (u)<-[:VISIT_AT]-(v:Visit)-[:VISIT_FORM]->(:Form {id:{formId}})
+    where v.status in {visit_status} with a, u, people, collect(v) as visits, collect(v.status) as status where not 2 in status or status is null
     with a, u{.*, people: people`+(req.query.formId?`, visits: visits`:``)+`} as unit
     with a, collect(unit) as units
   optional match (person:Person)-[:RESIDENCE {current:true}]->(a) `;
 
-      if (req.query.filter_key) q += `where ((a)<-[:RESIDENCE {current:true}]-(:Person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}}) or (person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})) `;
+      if (req.query.filter_key) q += `
+        where ((a)<-[:RESIDENCE {current:true}]-(:Person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})
+        or (person)<-[:ATTRIBUTE_OF {current:true}]-(:PersonAttribute {value:{filter_val}})-[:ATTRIBUTE_TYPE]->(:Attribute {id:{filter_key}})) `;
 
       if (req.query.filter_visited) q += (req.query.filter_key?`and`:`where`)+` not (person)<-[:VISIT_PERSON]-(:Visit)-[:VISIT_FORM]->(:Form {id:{formId}}) `;
 
       q += `
   optional match (attr:Attribute)<-[:ATTRIBUTE_TYPE]-(pattr:PersonAttribute)-[:ATTRIBUTE_OF {current:true}]->(person)
     with a, units, person, collect({id:attr.id, name:attr.name, value:pattr.value}) as attrs
-    with a, units, a.position as ap, collect(person{.*, attrs: attrs}) as people `;
+    with a, units, a.position as ap, collect(person{.*, attrs: attrs}) as people
+  optional match (a)<-[:VISIT_AT]-(v:Visit)-[:VISIT_FORM]->(:Form {id:{formId}})
+    where v.status in {visit_status} with a, units, ap, people, collect(v) as visits, collect(v.status) as status `;
 
-      if (req.query.formId) q += `optional match (a)<-[:VISIT_AT]-(v:Visit)-[:VISIT_FORM]->(:Form {id:{formId}}) where v.status in {visit_status} with a, units, ap, people, collect(v) as visits, collect(v.status) as status `;
+      if (!empty_addrs) q += `where (size(people) > 0 or size(units) > 0) and (not 2 in status or status is null) `;
 
-      if (!empty_addrs&&req.query.formId) q += `where (size(people) > 0 or size(units) > 0) and (not 2 in status or status is null) `;
-
-      q += `return collect({address: a{longitude:ap.x,latitude:ap.y,.id,.street,.city,.state,.zip,.updated}, units: units, people: people`+(req.query.formId?`, visits: visits`:``)+`}) as data`;
+      q += `return collect({address: a{longitude:ap.x,latitude:ap.y,.id,.street,.city,.state,.zip,.updated}, units: units, people: people, visits: visits}) as data`;
 
       ref = await req.db.query(q, req.query);
 
