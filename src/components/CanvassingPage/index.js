64a646857dc274c3bf319ff5afc161c3746761a7
Corey Henderson <corman@cormander.com>
2019-07-28 11:59:18 -0600

    do a POST to visitsAndPeople

--- src/components/CanvassingPage/index.js
+++ src/components/CanvassingPage/index.js
@@ -487,14 +487,17 @@ export default class App extends OVComponent {
     try {
       let https = true;
       if (this.state.server.match(/:8080/)) https = false;
-      let res = await fetch(
-        'http'+(https?'s':'')+'://'+this.state.server+API_BASE_URI+'/people/get/byposition?formId='+this.state.form.id+
-        '&longitude='+pos.longitude+'&latitude='+pos.latitude+
-        '&limit='+(canvassSettings.limit?canvassSettings.limit:100)+
-        (canvassSettings.filter_visited?'&filter_visited=home':'')+
-        (canvassSettings.filter_pins&&canvassSettings.filter_key?'&filter_key='+canvassSettings.filter_key+'&filter_val='+canvassSettings.filter_val:''),
-      {
-        method: 'GET',
+      let res = await fetch('http'+(https?'s':'')+'://'+this.state.server+API_BASE_URI+'/people/get/byposition', {
+        method: 'POST',
+        body: JSON.stringify({
+          formId: this.state.form.id,
+          longitude: pos.longitude,
+          latitude: pos.latitude,
+          limit: (canvassSettings.limit?canvassSettings.limit:100),
+          filter_visited: (canvassSettings.filter_visited?'home':undefined),
+          filter_key: (canvassSettings.filter_pins&&canvassSettings.filter_key?canvassSettings.filter_key:undefined),
+          filter_val: (canvassSettings.filter_val:undefined),
+        }),
         headers: {
           'Authorization': 'Bearer '+await _getApiToken(),
           'Content-Type': 'application/json',
