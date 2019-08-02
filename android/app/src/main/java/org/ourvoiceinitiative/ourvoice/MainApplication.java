e0033b71da51e4a76471601d4c57f988fcd338e4
Corey Henderson <corman@cormander.com>
2019-08-01 18:16:00 -0600

    fix androidX build for google-places

--- android/app/src/main/java/org/ourvoiceinitiative/ourvoice/MainApplication.java
+++ android/app/src/main/java/org/ourvoiceinitiative/ourvoice/MainApplication.java
@@ -3,6 +3,7 @@ package org.ourvoiceinitiative.ourvoice;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGooglePlacesPackage(),
             new NetInfoPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
