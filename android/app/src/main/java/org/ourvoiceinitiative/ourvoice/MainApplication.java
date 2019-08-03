2c9487330a395f22935fdda8059036a73002099b
Corey Henderson <corman@cormander.com>
2019-08-03 01:24:02 -0600

    add react-native-camera to build

--- android/app/src/main/java/org/ourvoiceinitiative/ourvoice/MainApplication.java
+++ android/app/src/main/java/org/ourvoiceinitiative/ourvoice/MainApplication.java
@@ -3,6 +3,7 @@ package org.ourvoiceinitiative.ourvoice;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new RNGooglePlacesPackage(),
             new NetInfoPackage(),
             new AsyncStoragePackage(),
