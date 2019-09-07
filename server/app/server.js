42e679b5625cbf6f2cd19660b3c6d0d6b1d166df
Corey Henderson <corman@cormander.com>
2019-09-07 08:56:55 -0600

    actually move the files

---
+++ server/app/server.js
@@ -0,0 +1,41 @@
+
+import https from 'https';
+import fs from 'fs';
+
+import { doStartupTasks } from './lib/startup';
+import { doExpressInit } from './createExpressApp';
+import { ov_config } from './lib/ov_config';
+import neo4j from './lib/neo4j';
+import queue from './lib/queue';
+
+const db = new neo4j(ov_config);
+const qq = new queue(db);
+
+db.query('return timestamp()')
+  .catch((e) => {
+    console.error("Unable to connect to database");
+    console.error(e);
+    process.exit(1)
+  })
+  .then(async () => {
+    await doStartupTasks(db, qq);
+
+    const app = doExpressInit(true, db, qq);
+
+    // Launch the server
+    if (ov_config.server_ssl_port && ov_config.server_ssl_key && ov_config.server_ssl_cert) {
+      console.log('express.js SSL startup');
+      https.createServer(
+        {
+          key: fs.readFileSync(ov_config.server_ssl_key),
+          cert: fs.readFileSync(ov_config.server_ssl_cert),
+        }, app
+      ).listen(ov_config.server_ssl_port);
+    } else {
+      const server = app.listen(ov_config.server_port, () => {
+        const { address, port } = server.address();
+        console.log('express.js startup');
+        console.log(`Listening at http://${address}:${port}`);
+      });
+    }
+  });
