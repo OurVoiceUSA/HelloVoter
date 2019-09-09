0213537b761563b37bb5c0664c81cb23d458aee6
Corey Henderson <corman@cormander.com>
2019-09-09 16:46:42 -0600

    initial commit of build script

---
+++ mobile/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# stop script on any failure
+set -e
+
+# clean entire build cache
+rm -rf node_modules ios/build ios/pods ios/Podfile.lock android/build android/app/build package-lock.json 
+
+# install dependancies
+npm install
+
+# iOS pods
+(
+  cd ios
+  pod repo update
+  pod install
+)
+
+# android release build
+(
+  cd android
+  ./gradlew assembleRelease
+)
+
+# build/launch iPhone simulator
+react-native run-ios
+
