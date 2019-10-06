1823443fc25df1172232b7084c61fcedf92bb53c
Corey Henderson <corman@cormander.com>
2019-10-06 12:52:05 -0600

    no need to glob in jest

--- mobile/__tests__/App-test.js
+++ mobile/__tests__/App-test.js
@@ -2,15 +2,8 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import glob from 'glob';
-
 //import App from '../src/App';
 // skipping due to StackNavigator causing issue on import
 it.skip('App renders correctly', () => {
   renderer.create(<App />);
 });
-
-// run all component tests
-glob
-  .sync('../src/**/*.test.js', { cwd: `${__dirname}/` })
-  .map(filename => require(`./${filename}`))
