3291829b8bc248bd577234730c10fd6d69872d6b
Corey Henderson <corman@cormander.com>
2019-10-11 21:48:46 -0600

    fix iOS invite deeplink when app is already open

--- mobile/App/InvitePage.js
+++ mobile/App/InvitePage.js
@@ -2,14 +2,17 @@
 import React, { PureComponent } from 'react';
 
 import {
-  Alert,
   Linking,
+  View,
 } from 'react-native';
 
-import { Spinner } from 'native-base';
+import { Container, Content, H3, Text, Spinner, Footer, FooterTab, Button } from 'native-base';
 
 import { StackActions, NavigationActions } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import storage from 'react-native-storage-wrapper';
+
+import { say } from './common';
 
 export default class App extends PureComponent {
 
@@ -17,35 +20,37 @@ export default class App extends PureComponent {
     super(props);
 
     this.state = {
+      error: false,
     };
-
   }
 
   componentDidMount() {
     Linking.addEventListener('url', this.handleOpenURL);
-    // Launched from an external URL
-    Linking.getInitialURL().then((url) => {
-      if (url) this.handleOpenURL({ url });
-      else {
-        Alert.alert('There was a problem', 'Completely close this app and try the invite link again. If trouble persists, ask for a QR Code and scan it from the canvassing screen.', [{text: 'OK'}], { cancelable: false });
-        this.goHome();
-      }
-    });
+
+    Linking.getInitialURL()
+    .then((url) => {
+      if (url) this.handleOpenURL({url});
+      else this.setError();
+    })
+    .catch(() => this.setError());
+  }
+
+  setError() {
+    this.setState({error: true});
   }
 
   componentWillUnmount() {
-    // Remove event listener
     Linking.removeEventListener('url', this.handleOpenURL);
   };
 
   handleOpenURL = async ({ url }) => {
     try {
       await storage.set('HV_INVITE_URL', url);
+      return this.goHome();
     } catch(e) {
       console.warn("handleOpenURL: "+e);
     }
-
-    this.goHome();
+    this.setError();
   }
 
   goHome() {
@@ -59,6 +64,26 @@ export default class App extends PureComponent {
   }
 
   render() {
+    if (this.state.error) return (
+      <Container>
+        <Content>
+          <H3>There was a problem</H3>
+          <Text></Text>
+          <Text>
+            Completely close this app and try the invite link again. If trouble persists,
+            ask for a QR Code and scan it from the canvassing screen.
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button onPress={() => this.goHome()}>
+              <Icon name="undo" size={25} />
+              <Text>{say("home")}</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
     return (<Spinner />);
   }
 
