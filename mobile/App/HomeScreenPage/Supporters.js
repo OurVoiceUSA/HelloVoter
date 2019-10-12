8df3e1995169049ff69671e806313b071695ed25
Corey Henderson <corman@cormander.com>
2019-10-11 23:08:03 -0600

    fix openDonate reference

--- mobile/App/HomeScreenPage/Supporters.js
+++ mobile/App/HomeScreenPage/Supporters.js
@@ -5,17 +5,17 @@ import {
 } from 'react-native';
 
 import { Text, Button, Spinner } from 'native-base';
-import { say } from '../common';
+import { say, openDonate } from '../common';
 
 export default Supporters = props => (
   <View>
-    <Button block dark transparent onPress={() => props.refer.openDonate()}>
+    <Button block dark transparent onPress={() => openDonate()}>
       <Text>{say("our_signature_supporters")}</Text>
     </Button>
     <Text></Text>
     <Text>{say("this_app_is_made_possible_by")}</Text>
     <Text></Text>
-    <Button block onPress={() => props.refer.openDonate()}>
+    <Button block onPress={() => openDonate()}>
       <Text>{say("support_us_on_patreon")}</Text>
     </Button>
     <Text></Text>
