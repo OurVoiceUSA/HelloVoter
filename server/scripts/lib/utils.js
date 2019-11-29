c81945aede87445e348829c6141daceb79ae1969
Corey Henderson <corman@cormander.com>
2019-11-29 14:01:39 -0700

    don't build neo4j, just pull it

--- server/scripts/lib/utils.js
+++ server/scripts/lib/utils.js
@@ -5,21 +5,12 @@ import { ov_config } from '../../app/lib/ov_config';
 
 export async function runDatabase(sandbox) {
   let docker = new Docker(new Options());
-  let ni = 'neo4j-hv';
-  let nc = ni+(sandbox?'-sandbox':'');
+  let ni = 'ourvoiceusa/neo4j-hv';
+  let nc = 'neo4j-hv'+(sandbox?'-sandbox':'');
 
   let d;
 
   try {
-    d = await docker.command('images');
-
-    if (d.images.filter(i => i.repository === ni).length === 0) {
-      console.log("Building the "+ni+" image, this may take several minutes.");
-      await docker.command("build --pull -t "+ni+" ../database");
-    } else {
-      console.log("Using cached local build of "+ni+" image.");
-    }
-
     d = await docker.command('ps');
 
     if (d.containerList.filter(i => i.names === nc).length === 0) {
