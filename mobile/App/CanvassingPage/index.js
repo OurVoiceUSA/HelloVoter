c1478cbb6de8a61a37a6319465b5cb102b03ab30
Corey Henderson <corman@cormander.com>
2019-12-22 14:58:21 -0700

    hide a toast before walkthrough, if any

--- mobile/App/CanvassingPage/index.js
+++ mobile/App/CanvassingPage/index.js
@@ -172,6 +172,7 @@ export default class App extends LocationComponent {
           list = walkthroughSettings;
           break;
       }
+      Toast.hide();
       startWalkthrough(list);
     };
 
