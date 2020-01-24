b81d0e0315bfba7d6f0ca214292c7b4fdc6f578c
Corey Henderson <corman@cormander.com>
2020-01-23 19:27:37 -0700

    need to push base up so it actually gets used in the pipeline

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ rm -rf node_modules package-lock.json
 ncu -u
 npm install
 
-sh client/build.sh
 sh database/build.sh
+docker push ourvoiceusa/neo4j-hv
+sh client/build.sh
 sh server/build.sh
 sh mobile/build.sh
 
