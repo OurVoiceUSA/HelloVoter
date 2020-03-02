3b895e59ceacea49630cbf93429ac3b34d069679
Corey Henderson <corman@cormander.com>
2020-03-02 13:44:19 -0700

    don't import android cert into ios

--- mobile/App/config.js
+++ mobile/App/config.js
@@ -3,5 +3,5 @@ import config from 'react-native-config';
 
 export const wsbase = config.WS_BASE;
 export const google_api_key = (Platform.OS === 'ios'?config.GOOGLE_API_KEY_IOS:config.GOOGLE_API_KEY_ANDROID);
-export const android_cert = config.ANDROID_CERT;
+export const android_cert = (Platform.OS === 'ios'?'':config.ANDROID_CERT);
 
