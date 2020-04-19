fda93c43eefd8c54c4c0a435252025ebc20b4fb4
Corey Henderson <corman@cormander.com>
2020-04-19 15:14:43 -0600

    bail if swagger parse fails

--- server/gen-swagger.sh
+++ server/gen-swagger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+set -e
 ./node_modules/swagger-jsdoc/bin/swagger-jsdoc.js -d ./app/lib/swaggerDef.v1.js $(find app/routes/api/v1 | grep -v test.js | grep '\.js$')  -o ./app/swagger.v1.json
 
--- server/start-nodemon.sh
+++ server/start-nodemon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./gen-swagger.sh
 
 exec node node_modules/@babel/node/lib/_babel-node app/server.js
