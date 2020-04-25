eff78d5830fb6bb560554d173bae55caeac6f30d
Corey Henderson <corman@cormander.com>
2020-04-25 15:06:15 -0600

    check redirect mobile vs. web

--- server/app/routes/api/v1/public/invite.test.js
+++ server/app/routes/api/v1/public/invite.test.js
@@ -6,6 +6,7 @@ import { appInit, base_uri, getObjs } from '../../../../../test/lib/utils';
 
 var api;
 var db;
+var mua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
 
 describe('Invite', function () {
 
@@ -31,11 +32,14 @@ describe('Invite', function () {
   it('invite URL desktop redirects to web', async () => {
     let r = await api.get(base_uri+'/public/invite');
     expect(r.statusCode).to.equal(302);
+    expect(r.headers.location).to.equal('https://ourvoiceusa.org/hellovoter/');
   });
 
-  it('invite URL desktop redirects to web with orgId', async () => {
-    let r = await api.get(base_uri+'/public/invite');
+  it('invite URL mobile redirects to mobile', async () => {
+    let r = await api.get(base_uri+'/public/invite')
+      .set('User-Agent', mua);
     expect(r.statusCode).to.equal(302);
+    expect(r.headers.location).to.equal('OurVoiceApp://invite?inviteCode=undefined&server=undefined');
   });
 
 });
