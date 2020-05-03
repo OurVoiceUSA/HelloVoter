f5c515629b8ebc3f59362eef696b01c08c9af57d
Corey Henderson <corman@cormander.com>
2020-05-03 11:50:33 -0600

    reference imports in route path by id, not filename

--- server/app/routes/api/v1/import.js
+++ server/app/routes/api/v1/import.js
@@ -14,36 +14,40 @@ module.exports = Router({mergeParams: true})
   if (!valid(req.body.filename)) return _400(res, "Invalid value to parameter 'filename'.");
   if (typeof req.body.attributes !== 'object') return _400(res, "Invalid value to parameter 'attributes'.");
 
-  // TODO: validate that req.body.filename is a file name
-  req.body.id = req.user.id;
   let ref = await req.db.query('match (a:ImportFile {filename:{filename}}) where a.submitted is not null return count(a)', req.body);
   if (ref[0] !== 0) return _403(res, "Import File already exists.");
 
   // attributes property stores which order they come in as
-  await req.db.query('match (a:Volunteer {id:{id}}) merge (b:ImportFile {filename:{filename}}) on create set b += {id: randomUUID(), created: timestamp(), attributes: {attributes}} merge (b)-[:IMPORTED_BY]->(a) with b unwind {attributes} as attr match (a:Attribute {name:attr}) merge (b)-[:ATTRIBUTES]->(a)', req.body);
+  ref = await req.db.query(`match (v:Volunteer {id:{id}})
+    create (if:ImportFile {id: randomUUID(), filename:{filename}, created: timestamp(), attributes: {attributes}})
+    create (if)-[:IMPORTED_BY]->(v)
+      with if
+    unwind {attributes} as attr
+    match (at:Attribute {name:attr}) merge (if)-[:ATTRIBUTES]->(at)
+    return if.id`, _.merge({}, req.body, req.user));
 
-  return res.json({});
+  return res.json({importId: ref[0]});
 })
-.get('/import/:filename', async (req, res) => {
+.get('/import/:id', async (req, res) => {
   if (req.user.admin !== true) return _403(res, "Permission denied.");
-  let imf = await req.db.query('match (if:ImportFile {filename:{filename}}) return if', req.params);
+  let imf = await req.db.query('match (if:ImportFile {id:{id}}) return if', req.params);
   return res.json(imf[0]);
 })
-.post('/import/:filename', async (req, res) => {
+.post('/import/:id', async (req, res) => {
   if (req.user.admin !== true) return _403(res, "Permission denied.");
 
   // TODO: verify data[0].length matches attriutes.length+8
   // TODO: verify each attribute exists
   // convert attriutes to part of a cypher query
   let attrq = "";
-  let ref = await req.db.query('match (if:ImportFile {filename:{filename}}) return if.attributes', req.params);
+  let ref = await req.db.query('match (if:ImportFile {id:{id}}) return if.attributes', req.params);
   for (let i = 0; i < ref[0].length; i++) {
     attrq += ',`'+ref[0][i]+'`:r['+(i+8)+']';
   }
   await req.db.query(`
-match (if:ImportFile {filename:{filename}})
+match (if:ImportFile {id:{id}})
 with collect(if) as lock call apoc.lock.nodes(lock)
-match (if:ImportFile {filename:{filename}})
+match (if:ImportFile {id:{id}})
 unwind {data} as r
 create (b:ImportRecord {pid:r[0], street:r[1], unit:r[2], city:r[3], state:r[4], zip:r[5], lng:r[6], lat:r[7]`+attrq+`})
 merge (b)-[:FILE]->(if)`,
@@ -51,19 +55,19 @@ merge (b)-[:FILE]->(if)`,
 
   return res.json({});
 })
-.put('/import/:filename', async (req, res) => {
+.put('/import/:id', async (req, res) => {
   if (req.user.admin !== true) return _403(res, "Permission denied.");
 
-  let ref = await req.db.query('match (if:ImportFile {filename:{filename}}) where if.submitted is null set if.submitted = timestamp() return count(if)', req.params);
+  let ref = await req.db.query('match (if:ImportFile {id:{id}}) where if.submitted is null set if.submitted = timestamp() return count(if)', req.params);
   if (ref[0] !== 1) return _403(res, "Import File already submitted for processing.");
 
-  let job = await req.qq.queueTask('doProcessImport', 'ImportFile {filename:{filename}}', req.params);
+  let job = await req.qq.queueTask('doProcessImport', 'ImportFile {id:{id}}', req.params);
 
   return res.json(job);
 })
-.delete('/import/:filename', async (req, res) => {
+.delete('/import/:id', async (req, res) => {
   // TODO: delete imported records as well
-  await req.db.query('match (if:ImportFile {filename:{filename}}) set if:DeletedImportFile remove if:ImportFile', req.params);
+  await req.db.query('match (if:ImportFile {id:{id}}) set if:DeletedImportFile remove if:ImportFile', req.params);
   return res.json({deleted: true})
 })
 .get('/imports', async (req, res) => {
