3703cc9f46fd98129eaf41bf8f4032d40091afd2
Corey Henderson <corman@cormander.com>
2020-05-08 17:49:19 -0600

    app store links on web version

--- client/src/screens/Canvassing.web.js
+++ client/src/screens/Canvassing.web.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Root, Content } from "../components/Layout";
 import { Button } from "../components/Buttons";
 
@@ -9,8 +9,11 @@ export const Canvassing = ({ navigation }) => {
   return (
     <Root>
       <Content>
-        <Text>Please download the mobile app</Text>
-        <Button to="/" title="Home" />
+        <Text>For canvassing features, please download our mobile app:</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <a href="https://play.google.com/store/apps/details?id=org.ourvoiceinitiative.ourvoice"><img alt="Google Play Store" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" height="62" width="158" /></a>
+          <a href="https://itunes.apple.com/us/app/our-voice-usa/id1275301651?ls=1&amp;mt=8"><img alt="Apple Store" src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" height="43" width="130" /></a>
+        </View>
       </Content>
     </Root>
   );
