2a34959852f5875b8fc559aa00f44fb819c54ac4
Corey Henderson <corman@cormander.com>
2020-05-08 20:34:27 -0600

    remove ported code

--- client/src/App/Native/common.js
+++ client/src/App/Native/common.js
@@ -28,18 +28,6 @@ import RBush from 'rbush';
 import rtree from '../rtree.json';
 import { geographies } from './geographies';
 
-export const STORAGE_KEY_JWT = 'OV_JWT';
-export const STORAGE_KEY_USERLOCAL = 'OV_USER';
-export const STORAGE_KEY_DISCLOSURE = 'OV_TERMS_2019_11_20';
-export const STORAGE_KEY_SETTINGS = 'OV_CANVASS_SETTINGS';
-export const STORAGE_KEY_OLDFORMS = 'OV_CANVASS_FORMS';
-export const STORAGE_KEY_RETRY = 'HV_RETRY_QUEUE';
-export const STORAGE_KEY_SERVERS = 'HV_SERVERS';
-export const URL_TERMS_OF_SERVICE = 'https://raw.githubusercontent.com/OurVoiceUSA/HelloVoter/master/docs/Terms-of-Service.md';
-export const URL_PRIVACY_POLICY = 'https://raw.githubusercontent.com/OurVoiceUSA/HelloVoter/master/docs/Privacy-Policy.md';
-export const URL_GUIDELINES = 'https://raw.githubusercontent.com/OurVoiceUSA/HelloVoter/master/docs/Canvassing-Guidelines.md';
-export const URL_HELP = 'https://github.com/OurVoiceUSA/HelloVoter/blob/master/docs/Canvassing.md';
-
 export const makeTooltipContent = text => (
   <View style={{paddingHorizontal: 24, paddingVertical: 8}}>
     <Text>{text}</Text>
@@ -70,10 +58,6 @@ export function sortAlphaNum(ao, bo) {
 }
 
 export const bbox_usa = {"type":"MultiPolygon","coordinates":[[[[-179,14],[-50,14],[-50,71],[-179,71],[-179,14]]]]};
-
-export function localaddress() {
-  return (Platform.OS === 'ios'?'localhost':'10.0.2.2');
-}
 
 export function getEpoch() {
   return Math.floor(new Date().getTime())
