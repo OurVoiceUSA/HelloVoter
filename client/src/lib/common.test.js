29c5e01d410b6bf8bbacc64d9c30e8bf7a002a5e
Corey Henderson <corman@cormander.com>
2020-05-16 10:54:00 -0600

    initial commit of common.test.js

---
+++ client/src/lib/common.test.js
@@ -0,0 +1,61 @@
+import { expect } from 'chai';
+
+import { STORAGE_KEY_JWT } from './consts';
+import { Platform } from './react-native';
+import * as storage from './storage';
+import * as common from './common';
+
+describe('common.js as ###OS###', function () {
+
+  before(async () => {
+    Platform.setOS('###OS###');
+  });
+
+  after(async () => {
+  });
+
+  it('localaddress is correct', async () => {
+    let addr = common.localaddress();
+    switch (Platform.OS) {
+      case 'android': expect(addr).to.equal('10.0.2.2'); break;
+      default: expect(addr).to.equal('localhost'); break;
+    }
+  });
+
+  // TODO: common.openURL(url, external)
+  // TODO: common.openGitHub(repo);
+  // TODO: common.openDonate()
+
+  it('_getApiToken with no token', async () => {
+    expect(await common._getApiToken()).to.equal("of the one ring");
+  });
+
+  it('_getApiToken with token', async () => {
+    await storage.set(STORAGE_KEY_JWT, 'foo');
+    expect(await common._getApiToken()).to.equal('foo');
+  });
+
+  it('api_base_uri', async () => {
+    expect(common.api_base_uri()).to.equal('/api/v1');
+  });
+
+  it('api_base_uri with OrgID', async () => {
+    expect(common.api_base_uri('DEMO')).to.equal('/DEMO/api/v1');
+  });
+
+  it('getEpoch is a large integer', async () => {
+    expect(common.getEpoch()).to.be.above(1588610882000);
+  });
+
+  // TODO: common.ingeojson(json, lng, lat) -- json.type of Polygon & MultiPolygon & default returns false
+
+  it('getUSState', async () => {
+    expect(common.getUSState({longitude: -118.3281370, latitude: 33.9208231})).to.be.equal('CA');
+  });
+
+  it('getUSState outisde USA', async () => {
+    expect(common.getUSState({longitude: 0, latitude: 0})).to.be.equal(undefined);
+  });
+
+});
+
