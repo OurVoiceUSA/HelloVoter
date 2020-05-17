85a8738297e5c71245142172b50460d0bfcf39cd
Corey Henderson <corman@cormander.com>
2020-05-17 11:00:33 -0600

    move map demo into routes

--- client/src/routes/Canvassing.native.js
+++ client/src/routes/Canvassing.native.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import { Text } from '../lib/react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-import { Root, Content } from '../components/Layout';
-import { Button } from '../components/Buttons';
-import NativeCanvassing from '../Native/map';
-import { Link } from '../lib/routing';
+export class Canvassing extends Component {
 
-export const Canvassing = ({ navigation }) => {
-  return (
-    <NativeCanvassing />
-  );
-};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refer: props.refer,
+      myPosition: {latitude: -118.3281370, longitude: 33.9208231},
+      region: {latitudeDelta: 0.004, longitudeDelta: 0.004},
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    const { myPosition, region } = this.state;
+
+    return (
+      <View style={styles.map}>
+            <Text>HELLO, WORLD!!</Text>
+
+            <MapView
+              ref={component => this.map = component}
+              initialRegion={{latitude: myPosition.latitude, longitude: myPosition.longitude, latitudeDelta: region.latitudeDelta, longitudeDelta: region.longitudeDelta}}
+              style={styles.map}
+              showsUserLocation={true}
+              followsUserLocation={false}
+              keyboardShouldPersistTaps={true}
+              showsIndoors={false}
+              showsTraffic={false}
+              {...this.props}>
+            </MapView>
+      </View>
+    );
+
+  }
+
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    justifyContent: 'center',
+    borderRadius: 10,
+    padding: 10,
+  },
+  iconContainer: {
+    backgroundColor: '#ffffff', width: 65, height: 65, borderRadius: 65,
+    borderWidth: 2, borderColor: '#000000',
+    alignItems: 'center', justifyContent: 'center', margin: 2.5,
+  },
+  turfInfoContainer: {
+    backgroundColor: '#ffffff', width: 125, height: 45,
+    borderWidth: 2, borderColor: '#000000',
+    alignItems: 'center', justifyContent: 'center', margin: 2.5,
+  },
+  map: {
+    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+  },
+});
