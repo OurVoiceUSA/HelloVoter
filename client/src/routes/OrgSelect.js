06c7d924ced438ec59743f2c47728e4af39126d5
Corey Henderson <corman@cormander.com>
2020-05-23 16:39:58 -0600

    remove unused imports

--- client/src/routes/OrgSelect.js
+++ client/src/routes/OrgSelect.js
@@ -1,10 +1,7 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import { Platform, Text } from '../lib/react-native';
+import { Text } from '../lib/react-native';
 
 import * as Layout from '../components/Layout';
-import { localaddress, openURL } from '../lib/common';
-import { URL_PRIVACY_POLICY } from '../lib/consts';
 import { Button } from '../components/Buttons';
 import { Heading } from '../components/Type';
 
