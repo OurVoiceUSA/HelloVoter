025170d7297e663d16521ffe83e9dcb5f8678988
Corey Henderson <corman@cormander.com>
2020-05-23 23:08:08 -0600

    remove duplicate code

--- client/src/routes/canvassing/index.js
+++ client/src/routes/canvassing/index.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from '../../lib/react-native';
+import { Text } from '../../lib/react-native';
 
-import { URL_APPLE_IOS, URL_GOOGLE_PLAY } from '../../lib/consts';
+import AppStoreButtons from '../../components/AppStoreButtons'
 import { Root, Content } from '../../components/Layout';
-import { openURL } from '../../lib/common';
 
-export default ({refer}) => {
+export default ({ refer }) => {
   return (
     <Root>
       <Content>
         <Text>For canvassing features, please download our mobile app:</Text>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <TouchableOpacity onPress={openURL.bind(refer, URL_GOOGLE_PLAY)}>
-            <img alt="Google Play Store" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" height="62" width="158" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={openURL.bind(refer, URL_APPLE_IOS)}>
-            <img alt="Apple Store" src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" height="43" width="130" />
-          </TouchableOpacity>
-        </View>
+        <AppStoreButtons refer={refer} />
       </Content>
     </Root>
   );
