cb8b0308818cf80e372f380bee92a00d18c58c21
Brian Jacokes <bjacokes@gmail.com>
2020-06-22 11:14:01 -0700

    Use async/await

--- server/app/routes/api/v1/payout.js
+++ server/app/routes/api/v1/payout.js
@@ -15,31 +15,40 @@ async function exchangeToken(req, res) {
   if (!req.body.token) return _400(res, "Invalid value to parameter 'token'.");
   if (!req.body.account_id) return _400(res, "Invalid value to parameter 'account_id'.");
 
-  var plaidClient = new plaid.Client(
+  const plaidClient = new plaid.Client(
     ov_config.plaid_client_id,
     ov_config.plaid_secret,
     ov_config.plaid_public_key,
     plaid.environments.sandbox,
   );
 
-  plaidClient.exchangePublicToken(req.body.token, function(err, res) {
-    if (err != null) {
-      return _400(res, err);
-    }
-    var accessToken = res.access_token;
-    // Generate a bank account token
-    plaidClient.createStripeToken(accessToken, req.body.account_id, async function(err, res) {
-      if (err != null) {
-        return _400(res, err);
-      }
-      var bankAccountToken = res.stripe_bank_account_token;
-      const customer = await stripe(ov_config.stripe_secret_key).customers.create({
-        source: bankAccountToken,
-        email: 'test_user@example.com',
-      });
-      console.log("customer: " + JSON.stringify(customer));
+  let plaidTokenRes;
+  try {
+    plaidTokenRes = await plaidClient.exchangePublicToken(req.body.token);
+  } catch (err) {
+    return _400(res, err);
+  }
+
+  const accessToken = plaidTokenRes.access_token;
+  // Generate a bank account token
+  let stripeTokenRes;
+  try {
+    stripeTokenRes = await plaidClient.createStripeToken(accessToken, req.body.account_id);
+  } catch (err) {
+    return _400(res, err);
+  }
+  
+  const bankAccountToken = stripeTokenRes.stripe_bank_account_token;
+  let customer;
+  try {
+    customer = await stripe(ov_config.stripe_secret_key).customers.create({
+      source: bankAccountToken,
+      email: 'test_user@example.com',
     });
-  });
+  } catch (err) {
+    return _400(res, err);
+  }
+  console.log("customer: " + JSON.stringify(customer));
 
   return res.json({});
 }
